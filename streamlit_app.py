58a2711abf595ff7d7e4285e83dadb126c059542
manjunath-vhm <42837269+manjunath-vhm@users.noreply.github.com>
2022-07-24 05:16:58 +0200

    Update streamlit_app.py
    more items and emojis added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit;
 
 streamlit.title('My Parents new Healthy Diner');
 streamlit.header('Breakfast Menu');
-streamlit.text('Omega 3 & Blueberry oat meal');
-streamlit.text('Kale, Spinach & Rocket Smoothie');
-streamlit.text('Gard boiled Free-Range eggs');
+streamlit.text('🥣 Omega 3 & Blueberry oat meal');
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie');
+streamlit.text('🐔 Hard boiled Free-Range eggs');
+streamlit.text('🥑🍞 Avacado Toast');
