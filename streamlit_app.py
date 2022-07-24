0a817dc782d5337f77f24250282a051f67cd478b
manjunath-vhm <42837269+manjunath-vhm@users.noreply.github.com>
2022-07-24 05:14:28 +0200

    Update streamlit_app.py
    added header and some texts for menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit;
 
 streamlit.title('My Parents new Healthy Diner');
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & Blueberry oat meal');
+streamlit.text('Kale, Spinach & Rocket Smoothie');
+streamlit.text('Gard boiled Free-Range eggs');
