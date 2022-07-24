8562b8071202ea76d449ac59bcd91bd40f80cad5
manjunath-vhm <42837269+manjunath-vhm@users.noreply.github.com>
2022-07-24 05:21:09 +0200

    Update streamlit_app.py
    adding panda library to read file in s3 to a dataframe and streaming the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit;
+import panda;
 
 streamlit.title('My Parents new Healthy Diner');
 streamlit.header('Breakfast Menu');
@@ -7,3 +8,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie');
 streamlit.text('🐔 Hard boiled Free-Range eggs');
 streamlit.text('🥑🍞 Avacado Toast');
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+streamlit.dataframe(my_fruit_list);
