7e7ca3d02f3d08396dab2009ee88863ba1deb7e4
manjunath-vhm <42837269+manjunath-vhm@users.noreply.github.com>
2022-07-25 16:01:19 +0200

    Update streamlit_app.py
    normalizing spi response and displaying it as a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,9 @@ streamlit.dataframe(fruits_to_show);
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# reads the json response and normalises it as text 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.text(fruityvice_normalized);
+# Normalised text converted to dataframe to be displayed in the form of a table
+streamlit.dataframe(fruityvice_normalized)
