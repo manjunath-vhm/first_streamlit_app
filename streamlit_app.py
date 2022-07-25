c6d1b9591b6ff4464d0aa7b3e01b802e29d9a48f
manjunath-vhm <42837269+manjunath-vhm@users.noreply.github.com>
2022-07-25 16:03:43 +0200

    Update streamlit_app.py
    removed json text display

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,6 @@ streamlit.dataframe(fruits_to_show);
 # Nex section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # reads the json response and normalises it as text 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
