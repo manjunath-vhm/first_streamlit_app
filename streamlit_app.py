df591f9dfecb17d19cdb32fef3f6df9b957cd0d9
manjunath-vhm <42837269+manjunath-vhm@users.noreply.github.com>
2022-07-25 16:05:17 +0200

    Update streamlit_app.py
    separating fruit name form the request URL string

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(fruits_to_show);
 
 # Nex section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 # reads the json response and normalises it as text 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
